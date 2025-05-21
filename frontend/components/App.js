8c5673252300f8fea5c60c6886afa0d1b57bb3ea
Nikk-Harvey <132252641+Nikk-Harvey@users.noreply.github.com>
2025-05-20 22:18:29 -0500

    Update App.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -8,11 +8,52 @@ const urlPeople = 'http://localhost:9009/api/people'
 function App() {
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+
+   const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const [peopleRes, planetsRes] = await Promise.all([
+          axios.get(urlPeople),
+          axios.get(urlPlanets),
+        ])
+
+        const people = peopleRes.data;
+        const planets = planetsRes.data;
+
+        const planetMap = {}
+        planets.forEach(planet => {
+          planetMap[planet.id] = planet
+        });
+
+        const mergedData = people.map(person => ({
+          ...person,
+          homeworld: planetMap[person.homeworld],
+        }))
+
+        setCharacters(mergedData)
+        setLoading(false)
+      } catch (err) {
+        console.log('Failed to fetch data:', err)
+      }
+    }
+    fetchData()
+  }, [])
+  
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {loading ? (
+        <p>Loading Characters...</p>
+      ) : (
+        characters.map(char => (
+          <Character key={char.id} character={char} />
+        ))
+      )}
     </div>
   )
 }
